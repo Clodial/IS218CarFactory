4825a38b0004c6dcd7102504cf3f493fb51ef0bb
Clodial <ff17cloud@gmail.com>
2015-10-19 01:33:34 -0400

    Made Page and Main Classes

--- app/app_class.php
+++ app/app_class.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+*
+*	Car Classes
+*
+**/
+
 class cl_car{
 
 	private $make;
@@ -17,6 +23,7 @@ class cl_car{
 		$this->hPower = $aPow;
 		$this->eCode = $aEngine;
 		$this->nCylin = $aCylin;
+
 	}
 
 
@@ -29,12 +36,291 @@ class cl_car{
 
 }
 
-
 class cl_carFactory{
 
 	public static function create($aMake, $aModel, $aPow, $aCylin, $aEngine, $aTrans){
 
 		return new cl_car($aMake, $aModel, $aCylin, $aPow, $aEngine, $aTrans);
+
+	}
+
+}
+
+/**
+*
+*	Page Classes
+*
+**/
+
+class cl_main{
+
+	private $cArray = [];
+
+	public function __construct($aCSV){
+
+		$this->cArray = $this->readCSV($aCSV);
+
+		$page_request = 'cl_carChoose';
+
+		if(!empty($_REQUEST)){
+
+			$page_request = $_REQUEST['page'];
+			if(!($page_request == "cl_carChoose") && !($page_request == "cl_carShow")){
+				$page_request = "cl_carChoose";
+			}
+		}
+
+		$page = new $page_request($this->getCSV());
+
+		if($_SERVER['REQUEST_METHOD'] == 'GET'){
+			
+			$page->get();
+		
+		}else{
+			
+			//defaulting to post, pretty much
+			$page->post();
+		
+		}
+
+	}
+
+	public function readCSV($aCsvFile){
+
+		$loc_LoT = array();
+		$i = 0;
+		$loc_handle = fopen($aCsvFile, 'r');
+
+		while(($row = fgetcsv($loc_handle, 1024)) !== false){
+
+			foreach($row as $k=> $value){
+
+				$loc_LoT[$i][$k] = strtoupper($value);
+
+			}
+
+			$i++;
+		}
+
+		fclose($loc_handle);
+		return $loc_LoT;
+
+	}
+
+	public function getCSV(){
+
+		return $this->cArray;
+	
+	}
+
+}
+
+class cl_page{
+
+	//This class doesn't get and post since, it isn't necessary
+	private $array_of_cars;
+
+	public function __construct($aArray){
+
+		$this->array_of_cars = $aArray;
+
+	}
+
+	public function getArray(){
+
+		return $this->array_of_cars;
+	
+	}
+
+}
+
+class cl_carChoose extends cl_page{
+
+	private $make_array = [];
+	private $model_array = [];
+	private $cylin_array = [];
+	private $horse_array = [];
+	private $engine_array = [];
+	private $trans_array = [];
+
+	public function get(){
+
+		$locArray = $this->getArray();
+
+		foreach($locArray as $row => $car){
+
+			$this->makeSelector($car, $this->make_array, 0);
+			$this->makeSelector($car, $this->model_array, 1);
+			$this->makeSelector($car, $this->cylin_array, 2);
+			$this->makeSelector($car, $this->horse_array, 3);
+			$this->makeSelector($car, $this->engine_array, 4);
+			$this->makeSelector($car, $this->trans_array, 5);
+		
+		}
+
+		$this->createForm($this->make_array,$this->model_array,$this->cylin_array,$this->horse_array,$this->engine_array,$this->trans_array);
+
+	}
+
+	public function post(){
+		
+		$locArray = $this->getArray();
+
+		foreach($locArray as $row => $car){
+
+			$this->make_array = $this->makeSelector($car, $this->make_array, 0);
+			$this->model_array = $this->makeSelector($car, $this->model_array, 1);
+			$this->cylin_array = $this->makeSelector($car, $this->cylin_array, 2);
+			$this->horse_array = $this->makeSelector($car, $this->horse_array, 3);
+			$this->engine_array = $this->makeSelector($car, $this->engine_array, 4);
+			$this->trans_array = $this->makeSelector($car, $this->trans_array, 5);
+		
+		}
+
+		$this->createForm($this->make_array,$this->model_array,$this->cylin_array,$this->horse_array,$this->engine_array,$this->trans_array);
+
+	}
+
+	public function makeSelector($aCSV, $aArrCh, $aIn){
+
+		if(!in_array(strtoupper($aCSV[$aIn]), $aArrCh)){
+
+			$aArrCh[] = strtoupper($aCSV[$aIn]);
+		
+		}
+
+		return $aArrCh;
+
+	}
+
+	public function createForm($aMake, $aModel, $aHorse, $aCylin, $aEngine, $aTrans){
+
+		echo '<div class="container">';
+		echo '	<form method="post">';
+		$this->createSelector("col-md-6", $aMake, "make");
+		$this->createSelector("col-md-6", $aModel, "model");
+		$this->createSelector("col-md-6", $aHorse, "horsepower");
+		$this->createSelector("col-md-6", $aCylin, "cylinders");
+		$this->createSelector("col-md-6", $aEngine, "engine");
+		$this->createSelector("col-md-6", $aTrans, "transmission");
+		echo ' 		<button type="submit" name="page" value="cl_carShow">Show Me Up</button>';
+		echo ' 	</form>';
+		echo '</div>';
+	
+	}
+
+	public function createSelector($aClass, $aArray, $aName){
+	
+		echo '<div class="item '. $aClass . '">';
+		echo '	<h1>Select ' . $aName . '</h1>';
+		echo '	<select class="item-select" name="' . $aName . '">';
+		echo ' 		<option value="*">All</option>';
+
+		foreach($aArray as $row => $part){
+			echo '	<option value="' . $part . '">' . $part . '</option>';
+		}
+
+		echo '	</select>';
+		echo '</div>';
+	
+	}
+}
+
+class cl_carShow extends cl_page{
+
+	private $cMake;
+	private $cModel;
+	private $cHorse;
+	private $cCylin;
+	private $cEngine;
+	private $cTrans;
+
+	public function get(){
+
+		if(!empty($_REQUEST)){
+			$this->cMake = $_REQUEST['make'];
+			$this->cModel = $_REQUEST['model'];
+			$this->cHorse = $_REQUEST['horsepower'];
+			$this->cCylin = $_REQUEST['cylinders'];
+			$this->cEngine = $_REQUEST['engine'];
+			$this->cTrans = $_REQUEST['transmission'];
+		}else{
+			$this->cMake = "*";
+			$this->cModel = "*";
+			$this->cHorse = "*";
+			$this->cCylin = "*";
+			$this->cEngine = "*";
+			$this->cTrans = "*";
+		}
+
+		$this->readCars("Make", $this->cMake, $this->getArray(), 0);
+		$this->readCars("Model", $this->cMake, $this->getArray(), 1);
+		$this->readCars("Horsepower", $this->cMake, $this->getArray(), 2);
+		$this->readCars("Cylinder Count", $this->cMake, $this->getArray(), 3);
+		$this->readCars("Engine Code", $this->cMake, $this->getArray(), 4);
+		$this->readCars("Trasmission", $this->cMake, $this->getArray(), 5);
+		echo '<form method="POST">';
+		echo ' 	<button type="submit" name="page" value="cl_carChoose">Show Me Down</button>';
+		echo '</form>';
+
+	}
+
+	public function post(){
+		
+		if(!empty($_REQUEST)){
+			$this->cMake = $_REQUEST['make'];
+			$this->cModel = $_REQUEST['model'];
+			$this->cHorse = $_REQUEST['horsepower'];
+			$this->cCylin = $_REQUEST['cylinders'];
+			$this->cEngine = $_REQUEST['engine'];
+			$this->cTrans = $_REQUEST['transmission'];
+		}else{
+			$this->cMake = "*";
+			$this->cModel = "*";
+			$this->cHorse = "*";
+			$this->cCylin = "*";
+			$this->cEngine = "*";
+			$this->cTrans = "*";
+		}
+
+		$this->readCars("Make", $this->cMake, $this->getArray(), 0);
+		$this->readCars("Model", $this->cModel, $this->getArray(), 1);
+		$this->readCars("Horsepower", $this->cHorse, $this->getArray(), 2);
+		$this->readCars("Cylinder Count", $this->cCylin, $this->getArray(), 3);
+		$this->readCars("Engine Code", $this->cEngine, $this->getArray(), 4);
+		$this->readCars("Transmission", $this->cTrans, $this->getArray(), 5);
+		echo '<br/>';
+		echo '<form class="container" method="POST">';
+		echo ' 	<button type="submit" name="page" value="cl_carChoose">Show Me Down</button>';
+		echo '</form>';
+		echo '<br/>';
+		echo '<br/>';
+		echo '<br/>';
+		echo '<br/>';
+
+	}
+
+	public function readCars($aName, $aVar, $aCars, $aInd){
+
+		echo '<div class="container">';
+		echo '<h1 class="col-md-12">By ' . $aName . ': ' . $aVar . '</h1>';
+
+		foreach($aCars as $row => $comp){
+
+			if( $aVar == "*" || ($aVar != "*" && $aVar == $comp[$aInd])){
+
+				$car = cl_carFactory::create($comp[0], $comp[1], $comp[2], $comp[3], $comp[4], $comp[5]);
+				echo '<div class="col-md-3">';
+				print_r($car->getMakeAndModel());
+				echo '<hr>';
+				echo '</div>';
+				unset($car);
+			
+			}
+
+		}
+		echo '</div>';
+		echo '<br/>';
 
 	}
 
--- index.php
+++ index.php
@@ -1,142 +1,19 @@
 <!doctype html>
-<?php
-include("inc/db_credentials.php");	//This is getting ready for PHP funsies
-include("app/app_class.php");
-include("app/app_func.php");
-include("app/db_connect.php");
-
-$fFile = "files/carCSV.csv";
-
-$array_of_cars = readCSV($fFile);
-
-$make_array = [];
-$model_array = [];
-$cylin_array = [];
-$horse_array = [];
-$engine_array = [];
-$trans_array = [];
-
-foreach($array_of_cars as $row => $car){
-	if(!in_array(strtoupper($car[0]), $make_array)){
-		$make_array[] = strtoupper($car[0]);
-	}
-	if(!in_array(strtoupper($car[1]), $model_array)){
-		$model_array[] = strtoupper($car[1]);
-	}
-	if(!in_array(strtoupper($car[2]), $horse_array)){
-		$horse_array[] = strtoupper($car[2]);
-	}
-	if(!in_array(strtoupper($car[3]), $cylin_array)){
-		$cylin_array[] = strtoupper($car[3]);
-	}
-	if(!in_array(strtoupper($car[4]), $engine_array)){
-		$engine_array[] = strtoupper($car[4]);
-	}
-	if(!in_array(strtoupper($car[5]), $trans_array)){
-		$trans_array[] = strtoupper($car[5]);
-	}
-}
-
-?>
 <html>
 <head>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<div class="container">
-	<form method="post">
-	<div class="item col-md-6">
-		<h1>Select Make</h1>
-		<select class="item-select" name="make">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($make_array); $i++){
-					echo '<option value="' . $make_array[$i] . '">' . $make_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<div class="item col-md-6">
-		<h1>Select Model</h1>
-		<select class="item-select" name="model">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($model_array); $i++){
-					echo '<option value="' . $model_array[$i] . '">' . $model_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<div class="item col-md-6">
-		<h1>Select Horsepower</h1>
-		<select class="item-select" name="horse">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($horse_array); $i++){
-					echo '<option value="' . $horse_array[$i] . '">' . $horse_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<div class="item col-md-6">
-		<h1>Select Number of Cylinders</h1>
-		<select class="item-select" name="cylin">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($cylin_array); $i++){
-					echo '<option value="' . $cylin_array[$i] . '">' . $cylin_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<div class="item col-md-6">
-		<h1>Select Engine Code</h1>
-		<select class="item-select" name="engine">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($engine_array); $i++){
-					echo '<option value="' . $engine_array[$i] . '">' . $engine_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<div class="item col-md-6">
-		<h1>Select Transmission</h1>
-		<select class="item-select" name="trans">
-			<option value="*">All</option>
-			<?php
-				for($i = 0; $i < count($trans_array); $i++){
-					echo '<option value="' . $trans_array[$i] . '">' . $trans_array[$i] . '</option>';
-				}
-			?>
-		</select>
-	</div>
-	<br/>
-	<br/>
-	<input type="submit" class="col-md-8 container"/>
-	</form>
-	</div>
-	<div class="container">
-		<?php
-if(isset($_POST["make"]) && isset($_POST["model"]) && isset($_POST["horse"]) && isset($_POST["cylin"]) && isset($_POST["engine"]) && isset($_POST["trans"])){
+<?php
+//include("inc/db_credentials.php");	//This is getting ready for PHP funsies
+include("app/app_class.php");
+//include("app/app_func.php");
+include("app/db_connect.php");
 
-	$fMake = $_POST["make"];
-	$fModel = $_POST["model"];
-	$fHorse = $_POST["horse"];
-	$fCylin = $_POST["cylin"];
-	$fEngine = $_POST["engine"];
-	$fTrans = $_POST["trans"];
+$fFile = "files/carCSV.csv";
 
-	readCars("Make", $fMake, $array_of_cars, 0);
-	readCars("Model", $fModel, $array_of_cars, 1);
-	readCars("Horsepower", $fHorse, $array_of_cars, 2);
-	readCars("Cylinder Count", $fCylin, $array_of_cars, 3);
-	readCars("Engine Code", $fEngine, $array_of_cars, 4);
-	readCars("Transmission", $fTrans, $array_of_cars, 5);
+$main = new cl_main($fFile);
 
-}
-		?>
-		<a href="index.php">Reset</a>
-	</div>
+?>	
 </body>
 </html>
